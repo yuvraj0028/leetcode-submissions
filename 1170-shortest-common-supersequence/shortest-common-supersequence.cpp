16a86d1d0d51015407638099b4d3a359da22f3e6
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2024-12-16 21:09:22 +0530

    Time: 56 ms (5.06%) | Memory: 26.7 MB (27.11%) - LeetSync

---
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n = str1.size(), m = str2.size();
+        string commonSeq = "";
+
+        // DP Table to store lcs
+        vector<vector<int > > dp(n+1, vector<int>(m+1));
+        
+        for(int i = n-1;i>=0;i--){
+            for(int j = m - 1; j>=0;j--){
+                int ans = 0;
+
+                if(str1[i] == str2[j]){
+                    ans = 1 + dp[i+1][j+1];
+                }
+
+                ans = max({ans, dp[i+1][j], dp[i][j+1]});
+                
+                dp[i][j] = ans;
+            }
+        }
+
+        int i = 0, j = 0;
+
+        while(i<n && j<m){
+            if(str1[i] == str2[j]){
+                commonSeq+=str1[i];
+                i++; j++;
+            } else if(dp[i+1][j] > dp[i][j+1]){
+                commonSeq+=str1[i];
+                i++;
+            } else {
+                commonSeq+=str2[j];
+                j++;
+            }
+        }
+
+        while(i<n) commonSeq+=str1[i++];
+        while(j<m) commonSeq+=str2[j++];
+
+        return commonSeq;
+    }
+};
