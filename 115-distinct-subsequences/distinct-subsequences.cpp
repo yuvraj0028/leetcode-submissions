eeae2dd5255762a9767e992966aaf8f9e720b417
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2024-12-17 00:34:14 +0530

    Time: 31 ms (60.38%) | Memory: 44.3 MB (15.19%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,3 +1,5 @@
+#define MOD 1000000007
+
 class Solution {
 private:
     int countDistinct(int i, int j, string &s, string &t, vector<vector<int > > &dp){
@@ -13,10 +15,30 @@ private:
 
         return dp[i][j] = countDistinct(i+1, j, s, t, dp);
     }
+
+    int countDistinctTabulation(string s, string t){
+        int n = s.size(), m = t.size();
+        vector<vector<long long > > dp(n+1, vector<long long>(m+1));
+
+        for(int i = 0; i<=n ;i++) dp[i][m] = 1;
+    
+        for(int i = n-1; i>=0; i--){
+            for(int j = m-1; j>=0; j--){
+                long long ans = 0;
+                if(s[i] == t[j]){
+                    ans = (dp[i+1][j+1]%MOD + dp[i+1][j])%MOD;
+                } else {
+                    ans = dp[i+1][j]%MOD;
+                }
+                dp[i][j] = ans%MOD;
+            }
+        }
+
+        return dp[0][0]%MOD;
+    }
+    
 public:
     int numDistinct(string s, string t) {
-        int n = s.size(), m = t.size();
-        vector<vector<int > > dp(n+1, vector<int>(m+1,-1));
-        return countDistinct(0, 0, s, t, dp);
+        return countDistinctTabulation(s, t);
     }
 };
