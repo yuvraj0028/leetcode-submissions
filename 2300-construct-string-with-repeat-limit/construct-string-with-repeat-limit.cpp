81f9df623c92abd0f14a4cc1aac872e98c31a663
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2024-12-17 22:08:09 +0530

    Time: 47 ms (49.62%) | Memory: 27.8 MB (52.21%) - LeetSync

--- 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     string repeatLimitedString(string s, int repeatLimit) {
+        // frequency array to store count of character
         vector<int> freq(26);
         string ans = "";
 
-        for(const char &c : s) freq[c-'a']++;
+        for(const char &c : s) freq[(int)c-97]++;
 
+        // traversing from last index to maintain lexicographically order
         for(int i = 25; i>=0 ;i--){
             int temp = repeatLimit;
-
+            
+            // storing character until repeatList is exausted
             while(freq[i] && temp){
                 ans+=(char)(i+97);
                 freq[i]--; temp--;
             }
 
+            // if still there are some character > repeatList
+            // we will find the character next to it and append it
+            // to the ans string and again store the same character
             if(freq[i]){
                 for(int j = i - 1; j>=0; j--){
                     if(freq[j]){
