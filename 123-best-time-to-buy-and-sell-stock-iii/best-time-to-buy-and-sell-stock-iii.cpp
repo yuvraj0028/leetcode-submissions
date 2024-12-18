971fde1cac7469050173745f76dcd9dfc2c8c303
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2024-12-18 19:33:10 +0530

    Time: 71 ms (59.14%) | Memory: 82.8 MB (70.57%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -48,9 +48,40 @@ private:
 
         return dp[0][1][2];
     }
+
+    int solveTabOptimized(vector<int>& prices){
+        int n = prices.size();
+
+        vector<vector<int > > curr(2, vector<int> (3));
+        vector<vector<int > > next(2, vector<int> (3));
+
+        for(int i = n-1; i>=0; i--){
+            for(int buy = 0; buy<2; buy++){
+                for(int limit = 1; limit<3; limit++){
+                    int profit = 0;
+
+                    if(buy){
+                        int buy_stock = -prices[i] + next[0][limit];
+                        int ignore = next[1][limit];
+                        profit = max(buy_stock, ignore);
+                    } else {
+                        int sell_stock = prices[i] + next[1][limit-1];
+                        int ignore = next[0][limit];
+                        profit = max(sell_stock, ignore);
+                    }
+
+                    curr[buy][limit] = profit;
+                }
+            }
+
+            next = curr;
+        }
+
+        return next[1][2];
+    }
 public:
     int maxProfit(vector<int>& prices) {
         // memset(dp, -1, sizeof(dp));
-        return solveTab(prices);
+        return solveTabOptimized(prices);
     }
 };
