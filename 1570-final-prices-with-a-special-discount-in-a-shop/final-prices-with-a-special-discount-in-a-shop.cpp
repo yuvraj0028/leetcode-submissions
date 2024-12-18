fa72f9a48da74a76eca0b6d71ae0a9842625efc2
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2024-12-18 12:50:12 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (9.86%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    vector<int> finalPrices(vector<int>& arr) {
-        int n = arr.size();
+    vector<int> finalPrices(vector<int>& prices) {
+        int n = prices.size();
         vector<int> ans(n);
+
+        // monotonic stack to store next smaller element
         stack<int> st;
 
         for(int i = n-1; i>=0; i--){
-
-            while(!st.empty() && arr[i]<st.top()) st.pop();
             
+            // pop element until we find a number which can be discounted
+            while(!st.empty() && prices[i]<st.top()) st.pop();
+            
+            // check if stack is empty directly assign the element
+            // else get the doscount and push to stack
             if(st.empty()){
-                ans[i] = arr[i];
-                st.push(arr[i]);
+                ans[i] = prices[i];
+                st.push(prices[i]);
             } else {
-                ans[i] = arr[i] - st.top();
-                st.push(arr[i]);
+                ans[i] = prices[i] - st.top();
+                st.push(prices[i]);
             }
         }
 
