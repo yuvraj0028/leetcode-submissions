b60409eff5af0387bba52b2f914203eec9e47a43
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2024-12-18 23:43:54 +0530

    Time: 7 ms (62.29%) | Memory: 15.1 MB (65.48%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -23,9 +23,35 @@ private:
         return dp[i][buy][k] = profit;
     }
 
+    int findProfitTabulation(int k, vector<int>& prices){
+        int n = prices.size();
+
+        for(int i = n-1; i>=0; i--){
+            for(int buy = 0; buy< 2; buy++){
+                for(int limit = 1 ; limit <= k; limit++){
+                    int profit = 0;
+
+                    if(buy){
+                        int buyStock = -prices[i] + dp[i+1][0][limit];
+                        int ignore = dp[i+1][1][limit];
+                        profit = max(buyStock, ignore);
+                    } else {
+                        int sellStock = prices[i] + dp[i+1][1][limit-1];
+                        int ignore = dp[i+1][0][limit];
+                        profit = max(sellStock, ignore);
+                    }
+
+                    dp[i][buy][limit] = profit;
+                }
+            }
+        }
+
+        return dp[0][1][k];
+    }
+
 public:
     int maxProfit(int k, vector<int>& prices) {
-        memset(dp, -1, sizeof(dp));
-        return findProfit(0, 1, k, prices);
+        memset(dp, 0, sizeof(dp));
+        return findProfitTabulation(k, prices);
     }
 };
