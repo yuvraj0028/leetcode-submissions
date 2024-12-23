f9716b16035410ef78c3696ec0e91fba12d43b4c
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2024-12-23 12:53:57 +0530

    Time: 162 ms (32.63%) | Memory: 14.6 MB (33.65%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -38,10 +38,33 @@ private:
 
     }
 
+    int findLisOptimized(vector<int>& nums){
+        int n = nums.size();
+        vector<int> curr(n+1), next(n+1);
+        
+        for(int i = n-1; i>=0; i--){
+            for(int prev = i-1; prev>=-1; prev--){
+                int include = 0;
+
+                if(prev == -1 || nums[prev] < nums[i]){
+                    include = 1 + next[i+1];
+                }
+
+                int exclude = next[prev + 1];
+            
+                curr[prev+1] = max(include, exclude);
+            }
+
+            next = curr;
+        }
+
+        return next[0];
+    }
+
 public:
     int lengthOfLIS(vector<int>& nums) {
         // int n = nums.size();
         // vector<vector<int > > dp(n+1, vector<int>(n+2,-1));
-        return findLisTabulation(nums);
+        return findLisOptimized(nums);
     }
 };
