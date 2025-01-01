0cd22927f9ac0d2514b582fbb341536e97bb161e
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2025-01-01 22:47:16 +0530

    Time: 11 ms (19.62%) | Memory: 27 MB (7.17%) - LeetSync

--- 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
@@ -2,28 +2,47 @@
 
 class Solution {
 private:
-    int dp[100001];
-    int countStrings(int minLength, int maxLength, int numZeros, int numOnes, int length){
-        if(length > maxLength){
-            return 0;
+    // int dp[100001];
+    // int countStrings(int minLength, int maxLength, int numZeros, int numOnes, int length){
+    //     if(length > maxLength){
+    //         return 0;
+    //     }
+
+    //     if(dp[length] != -1) return dp[length];
+
+    //     int got = 0;
+
+    //     if(length>=minLength && length <= maxLength){
+    //         got = 1;
+    //     }
+
+    //     int ones = countStrings(minLength, maxLength, numZeros, numOnes, length+numOnes) % MOD;
+    //     int zeros = countStrings(minLength, maxLength, numZeros, numOnes, length+numZeros) % MOD;
+
+    //     return dp[length] = (got + ones + zeros) % MOD;
+    // }
+
+    int countStringsTabulation(int minLength, int maxLength, int numZeros, int numOnes){
+        vector<int> dp(100001 + maxLength);
+
+        for(int length = maxLength; length>= 0; length--){
+            int got = 0;
+
+            if(length>=minLength && length <= maxLength){
+                got = 1;
+            }
+
+            int ones = dp[length+numOnes] % MOD;
+            int zeros = dp[length+numZeros] % MOD;
+
+            dp[length] = (got + ones + zeros) % MOD;
         }
 
-        if(dp[length] != -1) return dp[length];
-
-        int got = 0;
-
-        if(length>=minLength && length <= maxLength){
-            got = 1;
-        }
-
-        int ones = countStrings(minLength, maxLength, numZeros, numOnes, length+numOnes) % MOD;
-        int zeros = countStrings(minLength, maxLength, numZeros, numOnes, length+numZeros) % MOD;
-
-        return dp[length] = (got + ones + zeros) % MOD;
+        return dp[0];
     }
 public:
     int countGoodStrings(int minLength, int maxLength, int numZeros, int numOnes) {
-        memset(dp,-1, sizeof(dp));
-        return countStrings(minLength, maxLength, numZeros, numOnes, 0);
+        // memset(dp,-1, sizeof(dp));
+        return countStringsTabulation(minLength, maxLength, numZeros, numOnes);
     }
 };
