7ef0c0bdb7dc5a5481b8e6eca2ccdf5e5bff172b
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2025-01-03 00:17:39 +0530

    Time: 20 ms (74.30%) | Memory: 13.9 MB (32.16%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -18,17 +18,40 @@ private:
 
         return dp[i][j] = mini;
     }
-public:
-    int minCost(int n, vector<int>& cuts) {
-        int sz = cuts.size();
 
-        dp.resize(sz+3, vector<int>(sz+3,-1));
+    int findMinCostTabulation(int n, vector<int>& cuts){
+        int sz = cuts.size();
 
         cuts.push_back(n);
         cuts.insert(cuts.begin(), 0);
         sort(cuts.begin(), cuts.end());
 
-        return findMinCost(cuts, 1, sz);
+        for(int i = sz; i>0; i--){
+            for(int j = 1; j<=sz; j++){
+                if(i>j) continue;
+
+                int mini = INT_MAX;
+
+                for(int index = i; index<=j; index++){
+                    int cost = cuts[j+1] - cuts[i-1] 
+                        + dp[i][index-1]
+                        + dp[index+1][j];
+
+                    mini = min(cost, mini);
+                }
+
+                dp[i][j] = mini;
+            }
+        }
+
+        return dp[1][sz];
+    }
+public:
+    int minCost(int n, vector<int>& cuts) {
+        int sz = cuts.size();
+        dp.resize(sz+3, vector<int>(sz+3, 0));
+
+        return findMinCostTabulation(n, cuts);
 
     }
 };
