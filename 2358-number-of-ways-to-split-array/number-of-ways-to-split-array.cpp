fed8bdac8c50fc5e73b4d586dea2bc378351ab96
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2025-01-03 23:22:01 +0530

    Time: 6 ms (30.21%) | Memory: 89.4 MB (62.32%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
@@ -3,32 +3,17 @@ public:
     int waysToSplitArray(vector<int>& nums) {
         int n = nums.size();
 
-        long long leftSum = 0, rightSum = 0;
+        long long totalSum = accumulate(nums.begin(), nums.end(), 0LL);
 
-        long long ans = 0;
-
-        vector<long long> leftPrefix(n);
-        vector<long long> rightPrefix(n);
-
-        for(int i = 0; i<n; i++){
+        long long leftSum = 0, ans = 0;
+        
+        for(int i = 0; i<n-1; i++){
             leftSum+=nums[i];
+            long long rightSum = totalSum - leftSum;
 
-            leftPrefix[i] = leftSum;
+            if(leftSum >= rightSum) ans++;
         }
-
-        for(int i = n-1; i>=0; i--){
-            rightSum+=nums[i];
-
-            rightPrefix[i] = rightSum;
-        }
-
-        for(int i = 0;i<n-1; i++){
-            long long left = leftPrefix[i];
-            long long right = rightPrefix[i+1];
-
-            if(left >= right) ans++;
-        }
-
+        
         return ans;
     }
 };
