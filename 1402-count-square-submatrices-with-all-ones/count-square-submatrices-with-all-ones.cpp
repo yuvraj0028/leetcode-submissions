f8c9e67b2b8317d1f97fbd9e766ab71455f0af1e
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2025-01-04 18:59:21 +0530

    Time: 8 ms (44.90%) | Memory: 30.4 MB (26.40%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -17,6 +17,8 @@ public:
             dp[0][i] = matrix[0][i];
         }
 
+        // populating dp[i][j] based on
+        // left, up and prev diagonal
         for(int i = 1; i<n; i++){
             for(int j = 1; j<m; j++){
                 if(matrix[i][j] == 0){
@@ -24,16 +26,16 @@ public:
                 } else {
                     int left = dp[i][j-1];
                     int up = dp[i-1][j];
-
                     int diag = dp[i-1][j-1];
 
                     int mini = min({left, up, diag});
-
+                    
                     dp[i][j] = mini + 1;
                 }
             }
         }
 
+        // calculating the sum
         for(int i = 0; i<n ;i++){
             for(int j = 0; j<m; j++){
                 ans+=dp[i][j];
