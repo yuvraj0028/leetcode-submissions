99a3472aa07500615a52505362759b0333e54f4d
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2025-01-04 18:11:51 +0530

    Time: 1812 ms (6.91%) | Memory: 30.6 MB (21.35%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -29,10 +29,34 @@ private:
 
         return v[i] = mini;
     }
+
+    int findMinCutTabulation(string &s){
+        int n = s.size();
+        vector<int> v(n+1, 0);
+
+        for(int i = n-1; i>=0; i--){
+            string temp = "";
+            int mini = INT_MAX;
+
+            for(int j = i; j<s.size(); j++){
+                temp+=s[j];
+
+                if(isPalindrome(temp)){
+                    int cost = 1 + v[j+1];
+                    mini = min(mini, cost);
+                }
+            }
+
+            v[i] = mini;
+        }
+
+        return v[0] - 1;
+    }
+
 public:
     int minCut(string s) {
-        int n = s.size();
-        vector<int> v(n+1, -1);
-        return findMinCut(s, v, 0) - 1;
+        // int n = s.size();
+        // vector<int> v(n+1, -1);
+        return findMinCutTabulation(s);
     }
 };
