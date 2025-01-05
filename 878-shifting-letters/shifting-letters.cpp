796c64e9924dd4348ef2fd2cf6d33a551468987a
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2025-01-05 12:25:48 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (53.18%) - LeetSync

--- 878-shifting-letters/shifting-letters.cpp
+++ 878-shifting-letters/shifting-letters.cpp
@@ -3,14 +3,20 @@ public:
     string shiftingLetters(string s, vector<int>& shifts) {
         int n = s.size();        
         
+        // finding totalSum to add shifts from 
         long long totalSum = accumulate(shifts.begin(), shifts.end(), 0LL);
 
         for(int i = 0; i < n; i++){
+            // getting the ascii value of character
             long long ascii = s[i] - 97;
+
+            // adding shifts to character
             ascii += totalSum;
 
+            // reducing current shift for nxt characters
             totalSum-=shifts[i];
 
+            // replacing the character whith new shift
             s[i] = (char)((ascii % 26) + 97);
         }
 
