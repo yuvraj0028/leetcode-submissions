338c81716150279bc98ac0ca3c8441ac34cfb139
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2025-01-08 16:53:38 +0530

    Time: 2 ms (12.82%) | Memory: 8.5 MB (18.89%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,11 +1,65 @@
 class Solution {
+private:
+    vector<int> computeLps(string &s){
+        int n = s.size();
+
+        vector<int> lpsTable(n);
+
+        int i = 1;
+        int j = 0;
+
+        while(i<n){
+            if(s[i]==s[j]){
+                lpsTable[i] = j+1;
+                i++;
+                j++;
+            } else if(j>0){
+                j = lpsTable[j-1];
+            } else {
+                i++;
+            }
+        }
+
+        return lpsTable;
+    }
+
+    bool contains(string s, string goal, vector<int> &lpsTable){
+        int size = s.size();
+        int goalSize = goal.size();
+
+        int i = 0, j = 0;
+
+        while(i < size && j < goalSize){
+            if(s[i] == goal[j]){
+                i++;
+                j++;
+            }
+
+            if(i == size){
+                return true;
+            }
+
+            if(s[i] != goal[j]){
+                if(i>0){
+                    i = lpsTable[i-1];
+                } else {
+                    j++;
+                }
+            }
+        }
+
+        return false;
+    }
+
 public:
     bool rotateString(string s, string goal) {
-        if(goal.size() != s.size()) return false;
+        if(goal.size() != s.size())
+            return false;
         
+        vector<int> lpsTable = computeLps(s);
+
         goal+=goal;
 
-        return goal.find(s) != string::npos;
-
+        return contains(s, goal, lpsTable);
     }
 };
