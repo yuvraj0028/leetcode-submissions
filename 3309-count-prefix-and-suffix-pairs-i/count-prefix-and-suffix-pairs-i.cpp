e613d56ced62a2148eba84beaf8d62c671281397
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2025-01-08 15:38:03 +0530

    Time: 0 ms (100.00%) | Memory: 23.4 MB (78.18%) - LeetSync

---
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countPrefixSuffixPairs(vector<string>& words) {
+        int ans = 0;
+        int n = words.size();
+
+        for(int i = 0; i<n-1; i++){
+            
+            string pattern = words[i];
+            int patternSize = pattern.size();
+
+            for(int j = i+1; j<n; j++){
+                string word = words[j];
+                int wordSize = word.size();
+
+                if(word.find(pattern) == 0 && (word.rfind(pattern) == wordSize - patternSize)){
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
