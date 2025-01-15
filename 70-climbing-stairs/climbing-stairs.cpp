66746048fbc7ffc041c537290b94c686b53635c9
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2025-01-15 21:48:32 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (86.89%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -12,19 +12,16 @@ private:
     }
 
     int solveTB(int n){
-        vector<int> dp(n+1,0);
-
-        dp[0] = 1;
-        dp[1] = 1;
+        int prev1 = 1;
+        int prev2 = 1;
         
         for(int i = 2;i<=n;i++){
-            int oneStep = dp[i-1];
-            int twoSteps = dp[i-2];
-
-            dp[i] = oneStep + twoSteps;
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
 
-        return dp[n];
+        return prev1;
     }
 public:
     int climbStairs(int n) {
