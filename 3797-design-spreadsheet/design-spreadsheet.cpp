db11cc099bee6db25f53a4822c9847360309675f
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2025-09-19 18:33:47 +0530

    Time: 317 ms (10.10%) | Memory: 193.8 MB (13.83%) - LeetSync

---
+++ 3797-design-spreadsheet/design-spreadsheet.cpp
@@ -0,0 +1,94 @@
+class Spreadsheet {
+private:
+    unordered_map<char, int> colMapping;
+    vector<vector<int > > grid;
+
+    void populateColumnMap(){
+        int colValue = 0;
+        for(char i = 'A'; i<='Z'; i++){
+            char ch = (char)i;
+            colMapping[ch] = colValue;
+            colValue++;
+        }
+    }
+
+    pair<int,int> getRowColValueFromCell(string &cell){
+        string rowString = cell.substr(1);
+        char colLabel = cell[0];
+        
+        int row = stoi(rowString);
+        cout<<row;
+        int col = colMapping[colLabel];
+
+
+        pair<int,int> rowColPair = make_pair(row, col);
+        return rowColPair;
+    }
+
+    bool checkOperandIsCell(string &operand){
+        char ch = operand[0];
+        return colMapping.find(ch) != colMapping.end();
+    }
+
+    pair<int,int> parseFormulaString(string &formula){
+        int value1 = 0, value2 = 0;
+        int size = formula.size();
+        int index = formula.find("+");
+
+        string firstOperand = formula.substr(0,index);
+        string secondOperand = formula.substr(index+1);
+        
+        bool isFirstOperandCell = checkOperandIsCell(firstOperand);
+        bool isSecondOperandCell = checkOperandIsCell(secondOperand);
+
+        if(isFirstOperandCell){
+            pair<int,int> rowColPair = getRowColValueFromCell(firstOperand);
+            int row = rowColPair.first;
+            int col = rowColPair.second;
+
+            value1 = grid[row][col];
+        } else {
+            value1 = stoi(firstOperand);
+        }
+
+        if(isSecondOperandCell){
+            pair<int,int> rowColPair = getRowColValueFromCell(secondOperand);
+            int row = rowColPair.first;
+            int col = rowColPair.second;
+
+            value2 = grid[row][col];
+        } else {
+            value2 = stoi(secondOperand);
+        }
+
+        pair<int,int> values = make_pair(value1, value2);
+        return values;
+    }
+public:
+    Spreadsheet(int rows) {
+        populateColumnMap();
+        this->grid.resize(rows+1, vector<int>(26, 0));
+    }
+    
+    void setCell(string cell, int value) {
+        pair<int,int> rowColPair = getRowColValueFromCell(cell);
+        int row = rowColPair.first;
+        int col = rowColPair.second;
+
+        grid[row][col] = value;
+    }
+    
+    void resetCell(string cell) {
+        pair<int,int> rowColPair = getRowColValueFromCell(cell);
+        int row = rowColPair.first;
+        int col = rowColPair.second;
+
+        grid[row][col] = 0;
+    }
+    
+    int getValue(string formula) {
+        formula = formula.substr(1);
+        pair<int,int> values = parseFormulaString(formula);
+        return values.first + values.second;
+    }
+};
