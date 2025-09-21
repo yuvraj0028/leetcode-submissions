109ef8e210e4cfd8f31a2d93cd14e17e527e5d6d
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2025-09-21 18:19:58 +0530

    Time: 229 ms (97.81%) | Memory: 401.9 MB (78.83%) - LeetSync

---
+++ 2023-design-movie-rental-system/design-movie-rental-system.cpp
@@ -0,0 +1,86 @@
+#define movie_pair pair<int, pair<int,int>>
+
+class Compare{
+public:
+    bool operator()(const movie_pair &a, const movie_pair &b) const {
+        if (a.second.second != b.second.second)
+            return a.second.second < b.second.second;
+        if (a.first != b.first)
+            return a.first < b.first;
+        return a.second.first < b.second.first;
+    }
+};
+
+class MovieRentingSystem {
+private:
+    int shops;
+    unordered_map<int, set<movie_pair, Compare> > availableMovies;
+    unordered_map<long long, int> movieStoreToPrice;
+    set<movie_pair, Compare> rented;
+
+    void populateMovies(vector<vector<int>>& entries){
+        for(const vector<int> &entry : entries){
+            int shop = entry[0];
+            int movie = entry[1];
+            int price = entry[2];
+
+            movie_pair moviePair = {shop,{movie,price}};
+            long long key = encodedKey(shop,movie);
+            movieStoreToPrice[key] = price;
+            availableMovies[movie].insert(moviePair);
+        }
+    }
+
+    long long encodedKey(int shop, int movie){
+        return ((long long)shop << 32) | (unsigned int)movie;
+    }
+
+public:
+    MovieRentingSystem(int n, vector<vector<int>>& entries) {
+        shops = n;
+        populateMovies(entries);
+    }
+    
+    vector<int> search(int movie) {
+        if(availableMovies.find(movie) == availableMovies.end()) return {};
+        auto &moviesData = availableMovies[movie];
+        int maxMovies = 5;
+        vector<int> result;
+
+        for(const auto &moviePair: moviesData){
+            if(maxMovies < 1) break;
+            result.push_back(moviePair.first);
+            maxMovies--;
+        }
+        return result;
+    }
+    
+    void rent(int shop, int movie) {
+        long long key = encodedKey(shop,movie);
+        int price = movieStoreToPrice[key];
+        movie_pair moviePair = {shop,{movie,price}};
+        availableMovies[movie].erase(moviePair);
+        rented.insert(moviePair);
+    }
+    
+    void drop(int shop, int movie) {
+        long long key = encodedKey(shop,movie);
+        int price = movieStoreToPrice[key];
+        movie_pair moviePair = {shop,{movie,price}};
+        availableMovies[movie].insert(moviePair);
+        rented.erase(moviePair);
+    }
+    
+    vector<vector<int>> report() {
+        vector<vector<int > > result;
+        int maxMovies = 5;
+
+        for(const auto &moviePair: rented){
+            if(maxMovies < 1) break;
+            result.push_back({moviePair.first,moviePair.second.first});
+            maxMovies--;
+        }
+
+        return result;
+    }
+};
