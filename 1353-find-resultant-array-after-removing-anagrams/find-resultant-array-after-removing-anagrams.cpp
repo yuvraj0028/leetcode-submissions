dfc88885f5f4dadd6dacbc525a9a76389fc98244
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2025-10-13 23:24:18 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (88.15%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
@@ -1,41 +1,19 @@
 class Solution {
-private:
-    bool checkAnagrams(string &s1, string &s2){
-        if(s1.size() != s2.size()) return false;
-        multiset<char> ms;
-
-        for(const char &ch : s1){
-            ms.insert(ch);
-        }
-
-        for(const char &ch : s2){
-            auto it = ms.find(ch);
-            if(it != ms.end()){
-                ms.erase(it);
-            }
-        }
-
-        return ms.size() == 0;
-    }
-
 public:
     vector<string> removeAnagrams(vector<string>& words) {
-        int n = words.size();
-        
-        for(int i = 1; i<n; i++){
-            string s1 = words[i-1];
-            string s2 = words[i];
+        vector<string> result;
+        string prevSorted = "";
 
-            if(checkAnagrams(s1, s2)){
-                swap(words[i-1], words[i]);
-                words[i-1] = "";
+        for (const string &word : words) {
+            string sortedWord = word;
+            sort(sortedWord.begin(), sortedWord.end());  // sort characters of the word
+
+            if (sortedWord != prevSorted) {  // not an anagram of previous kept word
+                result.push_back(word);
+                prevSorted = sortedWord;
             }
         }
 
-        vector<string> ans;
-        for(const string &s : words)
-            if(s!="") ans.push_back(s);
-   
-        return ans;
+        return result;
     }
 };
