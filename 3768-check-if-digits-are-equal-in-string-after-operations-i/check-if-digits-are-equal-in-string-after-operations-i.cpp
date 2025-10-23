e00b463574be046f0a928cb85911108f969642d4
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2025-10-24 00:10:43 +0530

    Time: 11 ms (34.51%) | Memory: 12.2 MB (13.13%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -1,9 +1,8 @@
 class Solution {
-private:
-    bool solve(string &s){
+public:
+    bool hasSameDigits(string s) {
         if(s.size() == 2){
-            if(s[0] == s[1]) return 1;
-            return 0;
+            return s[0] == s[1] ? 1 : 0;
         }
 
         string newString = "";
@@ -11,11 +10,6 @@ private:
         for(int i = 0; i<s.size()-1; i++){
             newString += char((int(s[i] - '0') + int(s[i+1] - '0'))%10) + '0';
         }
-
-        return solve(newString);
-    }
-public:
-    bool hasSameDigits(string s) {
-        return solve(s);
+        return hasSameDigits(newString);
     }
 };
