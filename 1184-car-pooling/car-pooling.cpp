304edd734fd8ea6e24834a6db0ec52b09f4341ac
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2026-01-22 01:29:42 +0530

    Time: 3 ms (36.81%) | Memory: 14.7 MB (32.76%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -1,36 +1,23 @@
 class Solution {
-private:
-    static bool comparator(const vector<int> &tripA, const vector<int> &tripB) {
-        int fromTripA = tripA[1];
-        int toTripA = tripA[2];
-
-        int fromTripB = tripB[1];
-        int toTripB = tripB[2];
-
-        if(fromTripA == fromTripB){
-            return toTripA < toTripB;
-        }
-
-        return fromTripA < fromTripB;
-    }
-
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        sort(trips.begin(), trips.end(), comparator);
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int > > > pq;
+        vector<int> temp(1001, 0);
+
+        for(auto vec : trips){
+            int val = vec[0];
+            int from = vec[1];
+            int to = vec[2];
+
+            temp[from]+=val;
+            temp[to]-=val;
+        }
         
-        int prefix = 0;
+        int sum = 0;
 
-        for(int i = 0;i<trips.size(); i++){
-            while(!pq.empty() && pq.top().first<=trips[i][1]){
-                prefix-=pq.top().second;
-                pq.pop();
-            }
+        for(int i = 0; i<1001; i++){
+            sum+=temp[i];
 
-            pq.push({trips[i][2], trips[i][0]});
-            prefix+=trips[i][0];
-
-            if(prefix > capacity) return 0;
+            if(sum>capacity) return 0;
         }
 
         return 1;
