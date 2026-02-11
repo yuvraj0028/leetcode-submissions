b64f7514284f920ef498feb3ee71a02bac8f3b0f
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2026-02-12 00:56:41 +0530

    Time: 19 ms (13.56%) | Memory: 19 MB (67.08%) - LeetSync

---
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,53 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+private:
+    void solve(Node* root){
+        if(!root || !root->left) return;
+
+        root->left->next = root->right;
+
+        if(root->next){
+            root->right->next = root->next->left;
+        }
+
+        solve(root->left);
+        solve(root->right);
+    }
+
+    void printTree(Node* root){
+        if(!root) return;
+
+        if(root->next){
+            cout<<root->next->val<<endl;
+        } else {
+            cout<<"NULL"<<endl;
+        }
+
+        printTree(root->left);
+        printTree(root->right);
+    }
+
+public:
+    Node* connect(Node* root) {
+        solve(root);
+        printTree(root);
+        return root;
+    }
+};
