29e0e5e16eddc58e1160b0eef4447eabc7cbc66d
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2026-03-05 23:40:37 +0530

    Time: 3 ms (51.07%) | Memory: 20.1 MB (10.87%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,32 +1,27 @@
-class Comparator{
-public:
-    bool operator()(pair<int,int > &a, pair<int,int > &b){
-        return a.second > b.second;
-    }
-};
-
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        priority_queue<pair<int,int>, vector<pair<int,int > >, Comparator > pq;
+        int n = nums.size();
         unordered_map<int,int> mp;
 
-        for(const int &i : nums){
-            mp[i]++;
+        for(int i = 0; i<n; i++){
+            mp[nums[i]]++;
         }
 
-        for(const auto [key,val] : mp){
-            pq.push({key,val});
-
-            if(pq.size()>k){
-                pq.pop();
-            }
+        vector<vector<int > > bucket(n+1);
+        
+        for(const auto [key, val] : mp){
+            bucket[val].push_back(key);
         }
 
         vector<int> ans;
-        while(!pq.empty()){
-            ans.push_back(pq.top().first);
-            pq.pop();
+
+        for(int i = n; i>=0 && ans.size() < k; i--){
+            for(const auto &j : bucket[i]){
+                ans.push_back(j);
+
+                if(ans.size() == k) return ans;
+            }
         }
 
         return ans;
