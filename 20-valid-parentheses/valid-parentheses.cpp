85495e8c841680751ac9b61890d76f2fc536f9f7
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2026-03-13 13:12:29 +0530

    Time: 1 ms (18.61%) | Memory: 8.8 MB (67.09%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
+            int n = s.size();
+            stack<char> st;
 
-        for(int i = 0; i<s.size(); i++){
-            char ch = s[i];
-            if(ch == '(' || ch == '{' || ch == '['){
-                st.push(ch);
-            } else {
-                if(st.empty()) return false;
-                char top = st.top();
-                if((ch == ')' && top == '(') || (ch == ']' && top == '[') || (ch == '}' && top == '{')){
-                    st.pop();
+            for(int i = 0; i<n; i++) {
+                char ch = s[i];
+
+                if(ch == '[' || ch == '(' || ch == '{') {
+                    st.push(ch);
                 } else {
-                    return false;
+                    if (st.empty()) {
+                        return false;
+                    }
+
+                    char top = st.top();
+
+                    if((ch == ')' && top == '(') || (ch == '}' && top == '{') || (ch == ']' && top == '[')) {
+                        st.pop();
+                    } else {
+                        return false;
+                    }
                 }
             }
-        }
 
-        return st.empty();
+            return st.empty();
     }
 };
