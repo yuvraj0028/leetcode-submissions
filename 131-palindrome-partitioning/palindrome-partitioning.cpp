8f4d5075906ffbd5fabe86d78e21ed4f3ba574c4
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2026-06-19 16:11:23 +0530

    Time: 56 ms (41.43%) | Memory: 79.9 MB (24.33%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,36 +1,34 @@
 class Solution {
 private:
-    bool isPalindrome(string &s, int start, int end){
-        while(start <= end){
+    vector<vector<string>> ans;
+
+    bool isPalindrome(string &s, int start, int end) {
+        while(start <= end) {
             if(s[start++] != s[end--])
                 return false;
         }
-
         return true;
     }
 
-    void findPartitions(vector<vector<string > > &ans, vector<string> &v, string &s, int i){
-        if(i==s.size()){
-            ans.push_back(v);
+    void solve(string &str, int index, vector<string> &palindromes) {
+        if(index == str.size()) {
+            ans.push_back(palindromes);
             return;
         }
-    
-        for(int index = i; index<s.size(); index++){
-            if(isPalindrome(s,i,index)){
-                v.push_back(s.substr(i, index - i + 1));
-                findPartitions(ans, v, s, index+1);
-                v.pop_back();
+
+        for(int end = index; end < str.size(); end++) {
+            if(isPalindrome(str, index, end)) {
+                palindromes.push_back(str.substr(index, end - index + 1));
+                solve(str, end + 1, palindromes);
+                palindromes.pop_back();
             }
         }
     }
 
 public:
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> ans;
-        vector<string> v;
-
-        findPartitions(ans, v, s, 0);
-
+        vector<string> palindromes;
+        solve(s, 0, palindromes);
         return ans;
     }
 };
