4731941d855ec2ee97ce86c29f0d8772fdbe32de
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2026-07-01 22:12:12 +0530

    Time: 4 ms (77.92%) | Memory: 24.4 MB (28.20%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -13,11 +13,13 @@ private:
         int delRow[4] = {0,0,-1,1};
         int delCol[4] = {-1,1,0,0};
 
+        // bfs teaversals
         while(!q.empty()) {
             auto [r,c] = q.front();
             q.pop();
 
             for(int i = 0; i<4; i++) {
+                // finding neighbours
                 int row = r + delRow[i];
                 int col = c + delCol[i];
 
@@ -29,6 +31,8 @@ private:
                     !visited[row][col] && 
                     heights[row][col] >= heights[r][c]
                 ) {
+                    // valid neighbour -> curr height > previous height
+                    // store 2 for atlantic and 1 for pacific
                     visited[row][col] = 1;
                     if(flow[row][col] == INT_MAX) {
                         flow[row][col] = isPacific ? 1 : 2;
@@ -47,47 +51,62 @@ public:
         int n = heights.size();
         int m = heights[0].size();
 
+        // {row, col} -> pacific queue
         queue<pair<int,int>> pacificQueue;
+        // aux matrix to track the flow (1->pacific, 2->atlantic, 3->both, INT_MAX->None)
         vector<vector<int>> flow(n, vector<int>(m,INT_MAX));
+        // coords check for pacific & atlantic
         vector<vector<int>> visited(n, vector<int>(m,0));
+        // result DS -> stores coordinates(i,j) of value "3" from flow matrix
         vector<vector<int>> coords;
 
         // traverse for pacific
+        // fill left edges
         for(int row = 0; row<n; row++) {
             visited[row][0] = 1;
             pacificQueue.push({row,0});
-            flow[row][0] = flow[row][0] == INT_MAX ? 1 : 2;
+            // prefill ocean touching cells
+            flow[row][0] = 1;
         }
 
+        // fill top edges
         for(int col = 1; col<m; col++) {
             visited[0][col] = 1;
             pacificQueue.push({0,col});
-            flow[0][col] = flow[0][col] == INT_MAX ? 1 : 3;
+            // prefill ocean touching cells
+            flow[0][col] = 1;
 
         }
-
+        // bfs call for pacific
         bfs(pacificQueue, heights, flow, visited, n, m, 1);
 
-
         // traverse of atlantic
+        // {row, col} -> atlantic queue
         queue<pair<int,int>> atlanticQueue;
+        // clearing visited array for atlantic traversal
         visited.assign(n, vector<int>(m,0));
 
+        // fill right edge
         for(int row = 0; row<n; row++) {
             visited[row][m-1] = 1;
             atlanticQueue.push({row,m-1});
+            // prefill ocean touching cells
             flow[row][m-1] = flow[row][m-1] == INT_MAX ? 2 : 3;
 
         }
 
+        // fill bottom edge
         for(int col = 0; col<m-1; col++) {
             visited[n-1][col] = 1;
             atlanticQueue.push({n-1,col});
+            // prefill ocean touching cells
             flow[n-1][col] = flow[n-1][col] == INT_MAX ? 2 : 3;
         }
 
+        // bfs call for atlantic
         bfs(atlanticQueue, heights, flow, visited, n, m, 0);
 
+        // store the common water flowing cells (3)
         for(int i =0; i<n; i++) {
             for(int j =0;j<m; j++) {
                 if(flow[i][j] == 3) {
