0a5e9354d60f1f269de2e57954b67f8d3a750bcd
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2026-07-03 01:16:06 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (70.19%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -4,6 +4,7 @@ private:
         int n = board.size();
         int m = board[0].size();
 
+        // 'X' should not be traversed...
         if(i<0 || i>=n || j<0 || j>=m || board[i][j] == 'X' || visited[i][j]) {
             return;
         }
@@ -27,30 +28,37 @@ public:
         int m = board[0].size();
         vector<vector<int > > visited(n, vector<int>(m,0));
 
+        // check for left edges
         for(int i=0; i<n; i++) {
             if(board[i][0] == 'O' && !visited[i][0]) {
                 dfs(board, visited, i, 0);
             }
         }
 
+        // check for top edges
         for(int j=0; j<m; j++) {
             if(board[0][j] == 'O' && !visited[0][j]) {
                 dfs(board, visited, 0, j);
             }
         }
 
+        // check for right edges
         for(int i=0; i<n; i++) {
             if(board[i][m-1] == 'O' && !visited[i][m-1]) {
                 dfs(board, visited, i, m-1);
             }
         }
 
+        // check for bottom edges
         for(int j=0; j<m; j++) {
             if(board[n-1][j] == 'O' && !visited[n-1][j]) {
                 dfs(board, visited, n-1, j);
             }
         }
 
+        // now check if there are still 'O' post dfs
+        // means 'O' not visited
+        // if yes then convert them to 'X'
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(board[i][j] == 'O' && !visited[i][j]) {
