7309922a05f303b7a71d9f4410377936ea1276e6
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2026-07-14 22:38:29 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (29.47%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -13,12 +13,35 @@ private:
 
         return dp[i+1] = currCost + min(currStep, nextStep);
     }
+
+    int getMinCostTab(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n+1, 0);
+
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for(int i = 2; i<=n; i++) {
+            int step1 = dp[i-1] + cost[i-1]; 
+            int step2 = dp[i-2] + cost[i-2];
+        
+            dp[i] = min(step1, step2);
+        }
+
+        return dp[n];
+    }
+
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int> dp(n + 1, -1);
-        int initialStep = getMinCostRec(cost, -1, dp);
+        // recursive -> T.C - O(N), S.C - O(N)
+        // int n = cost.size();
+        // vector<int> dp(n + 1, -1);
+        // int initialStep = getMinCostRec(cost, -1, dp);
 
-        return initialStep;
+        // return initialStep;
+
+        // Tabular -> T.C - O(N), S.C - O(N)
+        return getMinCostTab(cost);
+
     }
 };
