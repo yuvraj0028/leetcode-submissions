dbd69a6699614be716379ae1520513882b2c8c96
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2026-07-14 09:25:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (57.75%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -11,6 +11,8 @@ private:
     }
 
     int climbStairsHelperTab(int n) {
+        if(n<=1) return 1;
+
         vector<int> dp(n+1, 0);
 
         dp[0] = 1;
@@ -24,9 +26,28 @@ private:
     }
 public:
     int climbStairs(int n) {
+        // recursive -> T.C=O(N) & S.C=O(N)
         // vector<int> dp(n+1, -1);
         // return climbStairsHelper(n, dp);
 
-        return climbStairsHelperTab(n);
+        // Tabular -> T.C=O(N) & S.C=O(N)
+        // return climbStairsHelperTab(n);
+
+        // Tabular Oprimized -> T.C=O(N) & S.C=O(1)
+
+        if(n<=1) return 1;
+
+        int prev = 1;
+        int prev2 = 1;
+        int curr = 0;
+
+        for(int i = 2; i<=n; i++) {
+            curr = prev + prev2;
+
+            prev2 = prev;
+            prev = curr;
+        }
+
+        return curr;
     }
 };
