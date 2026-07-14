7668a0d5e677080688c1f7aa15e5535865af03c5
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2026-07-14 10:18:05 +0530

    Time: 3 ms (8.51%) | Memory: 17.7 MB (29.47%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,24 +1,24 @@
 class Solution {
 private:
     int getMinCostRec(vector<int>& cost, int i, vector<int> &dp) {
-        if(i>=cost.size()) return 0;
+        // Cast cost.size() to int to prevent -1 from turning into UINT_MAX
+        if (i >= (int)cost.size()) return 0;
 
-        if(dp[i] != -1) return dp[i];
+        if(dp[i+1] != -1) return dp[i+1];
 
-        int currStep = cost[i] + getMinCostRec(cost, i+1, dp);
-        int nextStep = cost[i] + getMinCostRec(cost, i+2, dp);
+        int currCost = i == -1 ? 0 : cost[i];
 
-        return dp[i] = min(currStep, nextStep);
+        int currStep = getMinCostRec(cost, i+1, dp);
+        int nextStep = getMinCostRec(cost, i+2, dp);
+
+        return dp[i+1] = currCost + min(currStep, nextStep);
     }
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
         vector<int> dp(n + 1, -1);
-        int initialStep = getMinCostRec(cost, 0, dp);
+        int initialStep = getMinCostRec(cost, -1, dp);
 
-        dp.assign(n+1, -1);
-        int nextStep = getMinCostRec(cost, 1, dp);
-
-        return min(initialStep, nextStep);
+        return initialStep;
     }
 };
