bb43def0a60ae32c7189da07d53990ecc72f47cb
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2026-07-14 09:18:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (52.55%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,17 +1,32 @@
 class Solution {
 private:
-    int climbStairsHelper(int n, vector<int> &dp) {
+    int climbStairsHelperRec(int n, vector<int> &dp) {
         if(n<0) return 0;
 
         if(n==0) return 1;
 
         if(dp[n] != -1) return dp[n];
         
-        return dp[n] = climbStairsHelper(n-1, dp) + climbStairsHelper(n-2, dp);
+        return dp[n] = climbStairsHelperRec(n-1, dp) + climbStairsHelperRec(n-2, dp);
+    }
+
+    int climbStairsHelperTab(int n) {
+        vector<int> dp(n+1, 0);
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
-        return climbStairsHelper(n, dp);
+        // vector<int> dp(n+1, -1);
+        // return climbStairsHelper(n, dp);
+
+        return climbStairsHelperTab(n);
     }
 };
