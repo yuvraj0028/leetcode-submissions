190c043124f01ca33db2a7a67c2c28fd04192451
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2026-07-15 23:02:06 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (8.02%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,27 +1,46 @@
 class Solution {
 private:
-    int robHelperRec(vector<int>& nums, vector<int>& dp, int i, int n) {
-        if(i >= n) return 0;
+    int robHelperRec(vector<int>& nums, vector<vector<int>>& dp, int i, int n, bool firstRobbed) {
+        // Base case: out of bounds
+        if (i >= n) return 0;
 
-        if(dp[i] != -1) return dp[i];
+        // Return memoized result
+        if (dp[i][firstRobbed] != -1) return dp[i][firstRobbed];
 
-        int take = nums[i] + robHelperRec(nums, dp, i+2, n);
-        int notTake = robHelperRec(nums, dp, i+1, n);
+        // Option 1: Skip the current house
+        int notTake = robHelperRec(nums, dp, i + 1, n, firstRobbed);
 
-        return dp[i] = max(take, notTake);
+        // Option 2: Rob the current house
+        int take = 0;
+        
+        // If we are at the very last house, we can only rob it if we DID NOT rob house 0
+        if (i == n - 1) {
+            if (!firstRobbed) {
+                take = nums[i]; 
+            }
+        } else {
+            // If we are at house 0 and decide to rob it, we flag 'firstRobbed' as true for the rest of the branch
+            bool isFirstRobbed = (i == 0) ? true : firstRobbed;
+            take = nums[i] + robHelperRec(nums, dp, i + 2, n, isFirstRobbed);
+        }
+
+        // Memoize and return the max of both choices
+        return dp[i][firstRobbed] = max(take, notTake);
     }
+
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
+        
+        // Base case for a single house
+        if (n == 1) return nums[0];
 
-        if(n==1) return nums[0];
+        // 2D DP table: n rows, 2 columns (0 for false, 1 for true)
+        // dp[i][0] -> Max money from index i onwards if house 0 was NOT robbed
+        // dp[i][1] -> Max money from index i onwards if house 0 WAS robbed
+        vector<vector<int>> dp(n, vector<int>(2, -1));
 
-        vector<int> dp(n+1, -1);
-        int robFromFirst = robHelperRec(nums, dp, 0, n-1);
-
-        dp.assign(n+1, -1);
-        int robFromSecond = robHelperRec(nums, dp, 1, n);
-
-        return max(robFromFirst, robFromSecond);
+        // Start recursion at index 0, initially house 0 hasn't been robbed
+        return robHelperRec(nums, dp, 0, n, false);
     }
 };
