a8e328e8c452cb57a59ea6f1a88b6a2db119114c
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2026-07-17 08:39:52 +0530

    Time: 307 ms (15.96%) | Memory: 26.2 MB (25.50%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -11,26 +11,57 @@ private:
 
         return dp[i][j] = false;
     }
-public:
-    string longestPalindrome(string s) {
-        int maxLen = 0;
-        int startingIndex = 0;
 
+    string getPalindromeTab(string &s) {
         int n = s.size();
-        vector<vector<int > > dp(n+1, vector<int>(n+1, -1));
+        if (n == 0) return "";
 
-        for(int i = 0; i<n; i++) {
-            for(int j = 0; j<n; j++) {
-                int currLen = j-i+1;
-                if(checkPalindromeRec(s, i, j, dp)) {
-                    if(currLen > maxLen) {
-                        maxLen = currLen;
-                        startingIndex = i;
-                    }
+        vector<vector<bool > > dp(n+1, vector<bool>(n+1, false));
+        int maxLen = 1, startingIndex = 0;
+
+        for (int i = 0; i < n; i++) dp[i][i] = true;
+
+        for(int len = 2; len<=n; len++) {
+            for(int i = 0; i+len-1<n; i++) {
+                int j = i + len - 1;
+                if(s[i] == s[j]) {
+                    dp[i][j] = len == 2 ? true : dp[i+1][j-1];
+                } else {
+                    dp[i][j] = false;
+                }
+
+                if(dp[i][j] && len > maxLen) {
+                    maxLen = len;
+                    startingIndex = i;
                 }
             }
         }
 
         return s.substr(startingIndex, maxLen);
+    }
+
+public:
+    string longestPalindrome(string s) {
+        // int maxLen = 0;
+        // int startingIndex = 0;
+
+        // int n = s.size();
+        // vector<vector<int > > dp(n+1, vector<int>(n+1, -1));
+
+        // for(int i = 0; i<n; i++) {
+        //     for(int j = 0; j<n; j++) {
+        //         int currLen = j-i+1;
+        //         if(checkPalindromeTab(s, i, j, dp)) {
+        //             if(currLen > maxLen) {
+        //                 maxLen = currLen;
+        //                 startingIndex = i;
+        //             }
+        //         }
+        //     }
+        // }
+
+        // return s.substr(startingIndex, maxLen);
+    
+        return getPalindromeTab(s);
     }
 };
