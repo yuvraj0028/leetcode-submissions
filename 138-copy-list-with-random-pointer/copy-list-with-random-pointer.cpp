80ad11214fa83afe96e86b331d1d719db7eea618
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2026-08-10 21:55:13 +0530

    Time: 6 ms (54.63%) | Memory: 15.4 MB (8.58%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -33,44 +33,67 @@ private:
         }
     }
 
-public:
-    Node* copyRandomList(Node* head) {
-        if(!head) return nullptr;
-
-        int index = 0;
+    Node* deepCopySingleMap(Node* head) {
+        unordered_map<Node*, Node*> oldToNew;
         Node* curr = head;
 
         while(curr) {
-            nodeToIndex[curr] = index;
-            index++;
-            curr = curr->next;
+            oldToNew[curr] = new Node(curr->val);
+            curr=curr->next;
         }
 
-        Node* res = new Node(0);
-        Node* currRes = res;
-        
-        index = 0;
         curr = head;
-
         while(curr) {
-            if(indexToNode.find(index) != indexToNode.end()) {
-                currRes->next = indexToNode[index];
-                currRes = currRes->next;
+            oldToNew[curr]->next = oldToNew[curr->next];
+            oldToNew[curr]->random = oldToNew[curr->random];
 
-                mapRandomPointer(curr, currRes);
-            } else {
-                Node* newNode = new Node(curr->val);
-                currRes->next = newNode;
-                currRes = currRes->next;
-                indexToNode[index] = newNode;
-
-                mapRandomPointer(curr, currRes);
-            }
-            index++;
             curr = curr->next;
         }
 
-        return res->next;
+        return oldToNew[head];
+    }
+
+public:
+    Node* copyRandomList(Node* head) {
+        // if(!head) return nullptr;
+
+        // int index = 0;
+        // Node* curr = head;
+
+        // while(curr) {
+        //     nodeToIndex[curr] = index;
+        //     index++;
+        //     curr = curr->next;
+        // }
+
+        // Node* res = new Node(0);
+        // Node* currRes = res;
+        
+        // index = 0;
+        // curr = head;
+
+        // while(curr) {
+        //     if(indexToNode.find(index) != indexToNode.end()) {
+        //         currRes->next = indexToNode[index];
+        //         currRes = currRes->next;
+
+        //         mapRandomPointer(curr, currRes);
+        //     } else {
+        //         Node* newNode = new Node(curr->val);
+        //         currRes->next = newNode;
+        //         currRes = currRes->next;
+        //         indexToNode[index] = newNode;
+
+        //         mapRandomPointer(curr, currRes);
+        //     }
+        //     index++;
+        //     curr = curr->next;
+        // }
+
+        // return res->next;
+
+
+        return deepCopySingleMap(head);
 
     }
 };
