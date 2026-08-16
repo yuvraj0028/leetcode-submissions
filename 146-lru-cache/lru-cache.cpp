f897b224d6dfd1f35c800b27eb3058df431ebfbe
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2026-08-17 00:49:49 +0530

    Time: 11 ms (100.00%) | Memory: 48 MB (100.00%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -5,9 +5,11 @@ private:
     int size;
 
     void markRecentlyUsed(int key) {
-        nodes.erase(keyToNodeAddress[key].first);
-        nodes.push_front(key);
-        keyToNodeAddress[key].first = nodes.begin();
+        // nodes.erase(keyToNodeAddress[key].first);
+        // nodes.push_front(key);
+        // keyToNodeAddress[key].first = nodes.begin();
+
+        nodes.splice(nodes.begin(), nodes, keyToNodeAddress[key].first);
     }
 
 public:
@@ -16,25 +18,27 @@ public:
     }
     
     int get(int key) {
-        if(keyToNodeAddress.find(key) == keyToNodeAddress.end()) {
+        auto itr = keyToNodeAddress.find(key);
+        if(itr == keyToNodeAddress.end()) {
             return -1;
         }
 
-        int val = keyToNodeAddress[key].second;
+        int val = itr->second.second;
         markRecentlyUsed(key);
         return val;
     }
     
     void put(int key, int value) {
-        if(keyToNodeAddress.find(key) != keyToNodeAddress.end()) {
-            keyToNodeAddress[key].second = value;
+        auto itr = keyToNodeAddress.find(key);
+        if(itr != keyToNodeAddress.end()) {
+            itr->second.second = value;
             markRecentlyUsed(key);
             return;
-        } else {
-            nodes.push_front(key);
-            keyToNodeAddress[key] = {nodes.begin(), value};
-            size--;
         }
+
+        nodes.push_front(key);
+        keyToNodeAddress[key] = {nodes.begin(), value};
+        size--;
 
         if(size<0) {
             keyToNodeAddress.erase(nodes.back());
