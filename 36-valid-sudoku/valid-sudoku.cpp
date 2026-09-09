1da7d916672db40b0c74220a472775c023a96587
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2026-09-10 00:42:59 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (86.10%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,100 +1,27 @@
 class Solution {
-private:
-    bool checkValidBlock(int i, int j) {
-        if(
-            (i == 0 && j == 0) ||
-            (i == 0 && j == 3) || 
-            (i == 0 && j == 6) ||
-            (i == 3 && j == 0) ||
-            (i == 3 && j == 3) || 
-            (i == 3 && j == 6) ||
-            (i == 6 && j == 0) ||
-            (i == 6 && j == 3) || 
-            (i == 6 && j == 6)
-        ) {
-            return true;
-        }
-
-        return false;
-    }
-
-    bool checkBlock(int i, int j, vector<vector<char>>& board) {
-        vector<int> freq(10, 0);
-
-        for(int row = i; row<(i+3); row++) {
-            for(int col = j; col<j+3; col++) {
-                char ch = board[row][col];
-                if(ch != '.') {
-                    int num = ch - '0';
-                    freq[num]++;
-                }
-            }
-        }
-
-        for(const int &num : freq) {
-            if(num>1) return false;
-        }
-
-        return true;
-    }
-
-    bool checkRow(int col, vector<vector<char>>& board) {
-        vector<int> freq(10, 0);
-        int n = board.size();
-
-        for(int row = 0; row<n; row++) {
-            char ch = board[row][col];
-            if(ch != '.') {
-                int num = ch - '0';
-                freq[num]++;
-            }
-        }
-
-        for(const int &num : freq) {
-            if(num>1) return false;
-        }
-
-        return true;
-    }
-
-    bool checkCol(int row, vector<vector<char>>& board) {
-        vector<int> freq(10, 0);
-        int n = board.size();
-
-        for(int col = 0; col<n; col++) {
-            char ch = board[row][col];
-            if(ch != '.') {
-                int num = ch - '0';
-                freq[num]++;
-            }
-        }
-
-        for(const int &num : freq) {
-            if(num>1) return false;
-        }
-
-        return true;
-    }
-
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int n = board.size();
-        
-        for(int i = 0; i<n; i++) {
-            for(int j = 0;j<n; j++) {
-                if(checkValidBlock(i,j)) {
-                    if(!checkBlock(i,j,board)) {
-                        return false;
-                    }
-                }
+        int n = 9;
+        bool rows[10][10] = {false};
+        bool cols[10][10] = {false};
+        bool blocks[10][10] = {false};
+    
+        for(int row = 0; row<n; row++) {
+            for(int col = 0; col<n; col++) {
+                char ch = board[row][col];
 
-                if(!checkRow(j, board)) {
+                if(ch == '.') continue;
+
+                int num = ch - '0';
+                int block = (row/3) * 3 + (col/3);
+
+                if(rows[row][num] || cols[col][num] || blocks[block][num]) {
                     return false;
                 }
 
-                if(!checkCol(i, board)) {
-                    return false;
-                }
+                rows[row][num] = 1;
+                cols[col][num] = 1;
+                blocks[block][num] = 1;
             }
         }
 
