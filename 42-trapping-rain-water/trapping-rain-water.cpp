02306962d1acc42e6516f5efe5b722c522442552
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2026-09-16 02:58:21 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (77.71%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,22 +1,57 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        // int ans = 0;
+        // int n = height.size();
+        // vector<int> left(n), right(n);
+
+        // left[0] = height[0];
+        // for(int i = 1; i<n; i++) {
+        //     left[i] = max(left[i-1], height[i]);
+        // }
+
+        // right[n-1] = height[n-1];
+        // for(int i = n-2; i>=0; i--) {
+        //     right[i] = max(right[i+1], height[i]);
+        // }
+
+        // for(int i = 0; i<n; i++) {
+        //     ans+=min(left[i], right[i]) - height[i];
+        // }
+
+        // return ans;
+
+
+        // optimized two pointer approach
         int ans = 0;
         int n = height.size();
-        vector<int> left(n), right(n);
+        int left = 0, right = n-1;
+        int leftMax = 0, rightMax = 0;
 
-        left[0] = height[0];
-        for(int i = 1; i<n; i++) {
-            left[i] = max(left[i-1], height[i]);
-        }
-
-        right[n-1] = height[n-1];
-        for(int i = n-2; i>=0; i--) {
-            right[i] = max(right[i+1], height[i]);
-        }
-
-        for(int i = 0; i<n; i++) {
-            ans+=min(left[i], right[i]) - height[i];
+        while(left < right) {
+            // Case A if height left <= height right
+            if(height[left] <= height[right]) {
+                // if height left is > leftMax then update leftMax
+                // water cannot be store here
+                if(height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    ans += leftMax - height[left];
+                }
+                left++;
+            } else {
+                // case B if height left > height right
+                if(height[left] > height[right]) {
+                    // if height right is > rightMax then update rightMax
+                    // water cannot be store here
+                    if(height[right] >= rightMax) {
+                        rightMax = height[right];
+                    } else {
+                        ans += rightMax - height[right];
+                    }
+                    right--;
+                }
+            }
         }
 
         return ans;
