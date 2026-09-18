3be91a0b5a392f453847aef01ea4cc5cf8cb024b
Yuvraj Singh <96447724+yuvraj0028@users.noreply.github.com>
2026-09-19 01:44:57 +0530

    Time: 98 ms (7.48%) | Memory: 137.2 MB (34.74%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.cpp
+++ 1023-time-based-key-value-store/time-based-key-value-store.cpp
@@ -1,34 +1,44 @@
-class IntComparator{
-public:
-    bool operator()(const pair<string, int> &a, const pair<string, int> &b) const {
-        if (a.second != b.second) {
-            return a.second < b.second;
-        }
-        return a.first < b.first;
-    }
-};
+// class IntComparator{
+// public:
+//     bool operator()(const pair<string, int> &a, const pair<string, int> &b) const {
+//         if (a.second != b.second) {
+//             return a.second < b.second;
+//         }
+//         return a.first < b.first;
+//     }
+// };
 
 class TimeMap {
 private:
-    unordered_map<string, multiset<pair<string, int>, IntComparator> > store;
+    // unordered_map<string, multiset<pair<string, int>, IntComparator> > store;
+    unordered_map<string, vector<pair<string, int> > > store;
 public:
     TimeMap() {}
     
     void set(string key, string value, int timestamp) {
         pair<string, int> newPair = {value, timestamp};
-        store[key].insert(newPair);
+        store[key].push_back(newPair);
     }
     
     string get(string key, int timestamp) {
-        auto itr = store[key].lower_bound({"~", timestamp});
+        if(store.find(key) == store.end()) return "";
 
-        if(itr != store[key].begin()) {
-            --itr;
-            return itr->first;
+        int s = 0;
+        int e = store[key].size() - 1;
+
+        string res = "";
+
+        while(s<=e) {
+            int mid = (e-s)/2 + s;
+
+            if(store[key][mid].second <= timestamp) {
+                res = store[key][mid].first;
+                s = mid+1;
+            } else {
+                e = mid-1;
+            }
         }
-
-        return "";
-
+        return res;
     }
 };
 
